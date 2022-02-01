1d60939e469f5cc85718ced0dfe61b4f613c747a
dagarsa <danigarciasantes@gmail.com>
2022-02-01 18:40:45 +0100

    casi ejer 1 pract 2A

--- app/src/main/java/upv/dadm/quotesapp/DashboardActivity.java
+++ app/src/main/java/upv/dadm/quotesapp/DashboardActivity.java
@@ -3,6 +3,8 @@ package upv.dadm.quotesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button bGet = findViewById(R.id.bGet);
+        Button bFavourite = findViewById(R.id.bFavourite);
+        Button bSettings = findViewById(R.id.bSettings);
+        Button bAbout = findViewById(R.id.bAbout);
     }
 }
