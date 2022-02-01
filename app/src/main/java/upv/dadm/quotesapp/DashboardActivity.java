c3c1c19660c851d37ad0159b850f02b7fd76fcf8
dagarsa <danigarciasantes@gmail.com>
2022-02-01 19:17:24 +0100

    ejercicio 1 practica 2A

--- app/src/main/java/upv/dadm/quotesapp/DashboardActivity.java
+++ app/src/main/java/upv/dadm/quotesapp/DashboardActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -17,5 +19,34 @@ public class DashboardActivity extends AppCompatActivity {
         Button bFavourite = findViewById(R.id.bFavourite);
         Button bSettings = findViewById(R.id.bSettings);
         Button bAbout = findViewById(R.id.bAbout);
+
+        Log.d("mensaje", "estoy en dasboardActivity");
+
+        View.OnClickListener onClickMethod = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bGet.isPressed()){
+                    Toast.makeText(DashboardActivity.this, "Has pulsado " + bGet.getText(), Toast.LENGTH_LONG).show();
+                }
+                if (bFavourite.isPressed()){
+                    Toast.makeText(DashboardActivity.this, "Has pulsado " + bFavourite.getText(), Toast.LENGTH_LONG).show();
+                }
+                if (bSettings.isPressed()){
+                    Toast.makeText(DashboardActivity.this, "Has pulsado " + bSettings.getText(), Toast.LENGTH_LONG).show();
+                }
+                if (bAbout.isPressed()){
+                    Toast.makeText(DashboardActivity.this, "Has pulsado " + bAbout.getText(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+
+        bGet.setOnClickListener(onClickMethod);
+        bFavourite.setOnClickListener(onClickMethod);
+        bSettings.setOnClickListener(onClickMethod);
+        bAbout.setOnClickListener(onClickMethod);
+
+
+
     }
 }
